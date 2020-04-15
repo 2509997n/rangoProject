05eaa63c0c70d20bce2a39abd9e44894e0747bf6
2509997n <2509997n@student.gla.ac.uk>
2020-04-15 20:01:26 +0100

    chapter 4 complete

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -121,8 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATICFILES_DIRS = [STATIC_DIR, ]
 STATIC_URL = '/static/'
-STATICFILES_DIR = [STATIC_DIR, ]
+
 
 # Media files
 
