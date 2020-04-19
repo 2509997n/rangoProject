162adc7c7b72d256196fb705a35675a90f0bd651
2509997n <2509997n@student.gla.ac.uk>
2020-04-19 20:57:12 +0100

    6.2.3: index template - no categories present

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
 
 
 def index(request):
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {}
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
     return render(request, 'rango/index.html', context=context_dict)
     # return HttpResponse("Rango says hey there partner! '<a href=\'/rango/about/\'>About</a>'")
