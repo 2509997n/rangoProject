c0d87643915769a50e1f62dc3f7e7b334b52428b
2509997n <2509997n@student.gla.ac.uk>
2020-04-28 16:51:37 +0100

    7: add_category exercise complete

--- tango_with_django_project/rango/forms.py
+++ tango_with_django_project/rango/forms.py
@@ -1,25 +1,22 @@
 from django import forms
 from rango.models import Page, Category
 
-class CategoryForm(forms.ModelForms):
-    name = forms.CharField(max_length=128,
-                           help_text="Please enter the category name.")
-    views = forms.IntegerField(wideget=forms.HiddenInput(), initial=0)
-    likes = forms.IntegerField(wideget=forms.HiddenInput(), initial=0)
-    slug = forms.CharField(wideget=forms.HiddenInput(), required=False)
 
-    #an inline class to provide additional information in the form
+class CategoryForm(forms.ModelForm):
+    name = forms.CharField(max_length=128, help_text="Please enter the category name.")
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    slug = forms.CharField(widget=forms.HiddenInput(), required=False)
+
     class Meta:
         model = Category
         fields = ('name',)
 
 
 class PageForm(forms.ModelForm):
-    title = forms.CharField(max_length=128,
-                            help_text="Please enter the title of the page.")
-    url = forms.URLField(max_length=200,
-                         help_text="Please enter the URL of the page.")
-    views = forms.IntegerField(wideget=forms.HiddenInput(), initial=0)
+    title = forms.CharField(max_length=200, help_text="Please enter the title of the page.")
+    url = forms.URLField(max_length=200, help_text="Please enter the URL of the page.")
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
     class Meta:
         model = Page
